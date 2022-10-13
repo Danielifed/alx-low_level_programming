1b80b1c67d14453122a95a8d77874000dc8ca86f
Danielifed <d.danielife@gmail.com>
2022-10-13 07:01:40 -0700

    first code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * Descriptuib: main - prints out a stringe to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	code goes here
+	char mystr[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(mystr);
+	return (0);
+}
