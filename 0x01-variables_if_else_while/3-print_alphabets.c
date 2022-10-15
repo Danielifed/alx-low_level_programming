6be91721feb1d48deb455d1a90cf802306bfbaad
Danielifed <d.danielife@gmail.com>
2022-10-15 05:50:46 -0700

    lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints lower and uppercase letters
+ *
+ * Return: Return 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
