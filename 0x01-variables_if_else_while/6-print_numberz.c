7252189b052da986eee2187823643d47e3bed1db
Danielifed <d.danielife@gmail.com>
2022-10-15 06:54:39 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints numbers 0-9 without char or printf and puts
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
