f7cd443652b20e0d6bc4e1295b2d073302f4d5ba
Danielifed <d.danielife@gmail.com>
2022-10-18 10:25:15 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * islower - checks if a character is lowercase
+ * _islower - checks if a character is lowercase
  * @c: character to test
  * Description: checks if a character is a lowercase
  * Return: 1 if letter is lower case, 0 if not
