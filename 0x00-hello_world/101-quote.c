ca3589b286bfb27a9600e99a9fd9d2af15381edc
Danielifed <d.danielife@gmail.com>
2022-10-18 08:28:50 -0700

    starting afresh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
 /**
  * main - Entry point
+ *
  *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	-w("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
