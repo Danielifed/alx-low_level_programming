c4739d9d51497fd301363b00379862f8173c2559
Danielifed <d.danielife@gmail.com>
2022-10-18 10:21:34 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * Description: checks if a character is a lowercase
  * Return: 1 if letter is lower case, 0 if not
  */
-int islower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
