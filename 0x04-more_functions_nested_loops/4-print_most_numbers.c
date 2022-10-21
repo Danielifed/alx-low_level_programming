d99132f81c9d2078f2b61addb0269448bd02b582
Danielifed <d.danielife@gmail.com>
2022-10-20 21:19:11 -0700

    numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints 0 to 9
- *
- * Retun: 0-9, excluding 2 and 4, followed by new line
+ * print_most_numbers - prints the numbers 0 to 9
+ * without 2 and 4
+ * Retun: Always 0
  */
 void print_most_numbers(void)
 {
