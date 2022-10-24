5fad4083034bb4fcd3329110315d2a8f5d52a336
Danielifed <d.danielife@gmail.com>
2022-10-24 07:50:08 -0700

    prints string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - return the lenth of the string as an int value
+ * @s: parameter
+ *
+ * Retrun: Int value of the length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s++)
+		l++;
+	return (1);
+}
