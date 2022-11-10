001bdbd768653eb8b84c7414b8074971cd7c32d7
Danielifed <d.danielife@gmail.com>
2022-11-10 06:24:24 -0800

    min and max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 
 /**
+ * _abs - get absolute value
+ * @n: number to calculate
+ * Return: absolute value of the number
+ */
+int _abs(int n)
+{
+		n < 0 ? (n *= -1) : (n = n);
+			return (n);
+}
+/**
  * array_range - Create an arrary from min to max numbers.
  * @min: Minimum number.
  * @max: Maximum number.
@@ -10,25 +20,16 @@
  */
 int *array_range(int min, int max)
 {
-	int *tmp;
-	int size = (max - min) + 1, i;
+	int *p;
+	int range, i;
 
 	if (min > max)
-	{
 		return (NULL);
-	}
-
-	tmp = malloc(sizeof(int) * ((max - min) + 1));
-
-	if (tmp == NULL)
-	{
+	range = max - min + 1;
+	p = malloc(range * sizeof(*p));
+	if (p == NULL)
 		return (NULL);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		tmp[i] =  min;
-		min++;
-	}
-
-	return (tmp);
+	for (i = 0; i < range; min++, i++)
+		p[i] = min;
+	return (p);
+}
