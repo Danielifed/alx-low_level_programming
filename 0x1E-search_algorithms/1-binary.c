53bff6f5cadddc8baeaaaac3ebc5d35efe526802
Danielifed <d.danielife@gmail.com>
2023-05-10 14:53:17 -0700

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,53 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array
- * @array: A pointer to the first element of the array to search in
- * @size: The number of elements in array
- * @value: The value to search for
+ * search - performs a binary search
+ * @array: the array to search
+ * @right: the start index to search
+ * @left: the end index to search
+ * @value: the value to search for
  *
- * Return: The index where value is located, or -1 if not found
+ * Return: index if element is found else -1
+ */
+int search(int *array, size_t left, size_t right, int value)
+{
+	size_t u, mid = (left + right) / 2;
+	int numb;
+
+	if (left > right)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (u = left; u <= right; u++)
+	{
+		printf("%d", array[u]);
+		if (u != right)
+			printf(", ");
+	}
+	printf("\n");
+
+	numb = array[mid];
+
+	if (numb == value)
+		return (mid);
+	else if (numb > value)
+		return (search(array, left, mid - 1, value));
+	else
+		return (search(array, mid + 1, right, value));
+}
+
+/**
+ * binary_search - searches for a value in an array using the
+ * binary search algorithm
+ * @array: the array to search
+ * @size: the size of the array
+ * @value: the value to search for
+ *
+ * Return: index if the value found or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
+	if (!array)
 		return (-1);
-
-	size_t start = 0;
-	size_t end = size - 1;
-
-	while (start <= end)
-	{
-		size_t mid = (start + end) / 2;
-
-		printf("Searching in array: ");
-		for (size_t u = start; u <= end; u++)
-		{
-			printf("%d", array[u]);
-			if (u < end)
-				printf(", ");
-		}
-		printf("\n");
-
-		if (array[mid] == value)
-			return (mid);
-
-		if (array[mid] < value)
-			start = mid + 1;
-		else
-			end = mid - 1;
-	}
-
-	return (-1);
+	return (search(array, 0, size - 1, value));
 }
