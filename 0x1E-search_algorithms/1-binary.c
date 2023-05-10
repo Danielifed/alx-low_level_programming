ee9210ae52ec979df8158f6491dcd6f5aa63e4c4
Danielifed <d.danielife@gmail.com>
2023-05-10 14:26:09 -0700

    fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,40 +1,56 @@
 #include "search_algos.h"
 
 /**
- * binary_search - this will perform a binary search
- * @array: the array to be searched
- * @size: the size of index to search
- * @value: the value to be searched for
+ * binary_search - Searches for a value in a sorted array
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
  *
- * Return: index if element is found else -1
+ * Return: The index where value is located, or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-	{
+	size_t begin = 0, end = size - 1, mid;
+
+	if (!array)
 		return (-1);
-	}
 
-	int left = 0;
-	int right = size - 1;
-
-	while (left <= right)
+	while (begin <= end)
 	{
-		int mid = left + (right - left) / 2;
+		mid = (begin + end) / 2;
+		printf("Searching in array: ");
+		print_array(array, begin, end);
 
 		if (array[mid] == value)
-		{
 			return (mid);
-		}
+
 		else if (array[mid] < value)
-		{
-			left = mid + 1;
-		}
+			begin = mid + 1;
+
 		else
-		{
-			right = mid - 1;
-		}
+			end = mid - 1;
 	}
 
 	return (-1);
+}
+
+/**
+ * print_array - Prints an array of integers
+ * @array: A pointer to the first element of the array to print
+ * @left: The leftmost index to print
+ * @right: The rightmost index to print
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t u;
+
+	for (u = begin; u <= end; i++)
+	{
+		printf("%d", array[u]);
+
+		if (begin != end)
+			printf(", ");
+	}
+
+	printf("\n");
 }
